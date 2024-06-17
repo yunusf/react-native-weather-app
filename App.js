1a7b2f64a24bb5ecdda6149c0df47e2ca1dc6db2
yunusf <yunusf42@gmail.com>
2024-06-18 03:50:25 +0700

    added render component, like loading status, success status, and error status

--- App.js
+++ App.js
@@ -2,16 +2,36 @@ import React, { useState } from 'react'
 import axios from 'axios'
 import { BASE_URL, API_KEY } from './src/constant'
 
-import { StyleSheet, View } from 'react-native'
+import { StyleSheet, View, ActivityIndicator, Text } from 'react-native'
 import WeatherSearch from './src/components/weatherSearch'
 import WeatherInfo from './src/components/weatherInfo'
 
 export default function App() {
   // Definisikan state "weatherData" dan "setWeatherData"
   const [weatherData, setWeatherData] = useState()
+
+  const [status, setStatus] = useState('')
+
+  const renderComponent = () => {
+    switch (status) {
+      case 'loading':
+        return <ActivityIndicator size={"large"} />
+      case 'success':
+        return <WeatherInfo weatherData={weatherData} />
+      case 'error':
+        return (
+          <Text>
+            Something went wrong. Please try again with a correct city name.
+          </Text>
+        )
+      default:
+        return
+    }
+  }
   
   // Perbarui function searchWeather dengan menggunakan axios
   const searchWeather = (location) => {
+    setStatus('loading')
     axios
       .get(`${BASE_URL}?q=${location}&appid=${API_KEY}`)
       .then(response => {
@@ -21,8 +41,9 @@ export default function App() {
         data.main.temp -= 273.15 // konversi kelvin ke celcius
         data.main.temp = data.main.temp.toFixed(2)
         setWeatherData(data)
+        setStatus('success')
       })
-      .catch(err => console.log(err))
+      .catch(err => setStatus('error'))
   }
 
   return (
@@ -31,13 +52,17 @@ export default function App() {
       <WeatherSearch searchWeather={searchWeather} />
 
       {/* Tampilkan data cuaca ketika ada weatherData */}
-      {weatherData && <WeatherInfo weatherData={weatherData} />}
+      {/* {weatherData && <WeatherInfo weatherData={weatherData} />} */}
+      <View style={styles.marginTop20}>{ renderComponent() }</View>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
-    padding:20,
+    padding: 20,
   },
+  marginTop20: {
+    marginTop: 20,
+  }
 })
